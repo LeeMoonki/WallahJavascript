ddc27e4aa82ca51f21074ca6728745b176473c7d
MoonKee Lee <moonkeelee@MoonKeeui-MacBook-Pro.local>
2018-01-04 23:15:53 +0900

    20180104

---
+++ JavaScriptFunc/BValue/BValueUsage.js
@@ -0,0 +1,39 @@
+var users = [
+    { id: 1, name: "ID", age: 32 },
+    { id: 2, name: "HA", age: 25 },
+    { id: 3, name: "BJ", age: 41 },
+    { id: 4, name: "PJ", age: 28 },
+    { id: 5, name: "JE", age: 27 },
+    { id: 6, name: "JM", age: 33 },
+    { id: 7, name: "HI", age: 24 }
+];
+
+function filter(list, predicate) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        if (predicate(list[i])) {
+            new_list.push(list[i]);
+        }
+    }
+    return new_list;
+}
+
+function map(list, iteratee) {
+    var new_list = [];
+    for (var i = 0, len = list.length; i < len; i++) {
+        new_list.push(iteratee(list[i]));
+    }
+    return new_list;
+}
+
+function bvalue(key) {
+    return function(obj) {
+        return obj[key];
+    }
+}
+
+console.log(map(
+    filter(users, function (user) { 
+        return user.age >= 30; 
+    })
+    , bvalue("name")));
