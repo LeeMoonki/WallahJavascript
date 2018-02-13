2812d8fb06d5c6b362193d580c254566b2b246ca
MoonKee Lee <shinbaek89@gmail.com>
2018-02-13 23:53:42 +0900

    compose

--- JavaScriptFunc/FunctionalFunctions.js
+++ JavaScriptFunc/FunctionalFunctions.js
@@ -112,3 +112,17 @@ function some(list) {
 function every(list) {
     return beq(negativeIndex(list));
 }
+
+// compose
+function compose() {
+    var args = arguments;
+    var start = args.length - 1;
+    return function() {
+        var i = start;
+        var result = args[start].apply(this, arguments);
+        while (i--) {
+            result = args[i].call(this, result);
+        }
+        return result;
+    }
+}
