d9c1e809f7134821a87c4800ff6d10057147e212
unknown <shinbaek89@gmail.com>
2018-06-21 19:01:10 +0900

    promise

--- JavaScript/BasicAndDeep/Promise/promise_usage2_single_value.js
+++ JavaScript/BasicAndDeep/Promise/promise_usage2_single_value.js
@@ -35,3 +35,53 @@ Promise.resolve({
 }).then((value) => {
     console.log("object argument : ", value); // { one: 1, two: 2, three: 3 }
 });
+
+
+// resolve 하는 value는 하나이기 때문에 
+// 여러 값을 하나로 묶는 작업이 필요하다.
+// 단, 전달하는 값 중에 비동기 작업이 존재한다면 이걸 또 Promise로 처리하고 다시 묶는 작업이 필요한데
+// 다음 두 가지 방법이 있다.
+
+// y는 x의 3배를 하는 작업
+function getY(x) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() { resolve(3 * x); }, 100);
+    });
+}
+
+// 1.
+
+function foo1(a, b) {
+    var x = a * b;
+
+    return getY(x)
+    .then(function(y) {
+        return [ x, y ];
+    });
+}
+
+foo1(10, 20)
+.then(function(msgs) {
+    var x = msgs[0];
+    var y = msgs[1];
+    console.log(x, y); // 200 600
+});
+
+
+// 2.
+
+function foo2(a, b) {
+    var x = a * b;
+
+    return [
+        Promise.resolve(x),
+        getY(x)
+    ];
+}
+
+Promise.all(foo2(10, 20))
+.then(function(msgs) {
+    var x = msgs[0];
+    var y = msgs[1];
+    console.log(x, y); // 200 600
+});
