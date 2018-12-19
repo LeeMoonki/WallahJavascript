3b93423d18efb973ac0c08de252e3ab2a20a5d76
LeeMoonki <ansrlansrl@xinapse.ai>
2018-12-19 11:38:37 +0900

    revision nextChain

--- JavaScript/DesignPattern/NextChainPattern/NextChainPattern.js
+++ JavaScript/DesignPattern/NextChainPattern/NextChainPattern.js
@@ -1,6 +1,7 @@
 const nextChain = function(chains) {
-    let flag = true, len = chains.length, i = 0;
+    // 여기에 반복 조건을 두게되면 closure이기 때문에 제대로 동작하지 않는다.
     return function(to, from, next) {
+        let flag = true, len = chains.length, i = 0;
         while(flag && i < len) {
             flag = false;
             if (i + 1 === len) {
